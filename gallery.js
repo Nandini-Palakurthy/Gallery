c69a1fb687177532decda90d2a96fbc9bf113939
Nandini-Palakurthy <131530342+Nandini-Palakurthy@users.noreply.github.com>
2023-04-23 13:29:08 +0530

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,13 @@
+function colOfImg()
+{
+    var allImgTag=document.images;
+    for(i=0;i<allImgTag.length;i++)
+    {
+        allImgTag[i].src=`${i}.jpg`
+    }
+}
+colOfImg()
+    function bigWindow(imgUrl)
+    {
+        document.getElementById("imgContainer").style.backgroundImage=`url(${imgUrl})`
+    }
